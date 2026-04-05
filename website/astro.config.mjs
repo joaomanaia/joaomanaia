520b28068ed2bff68262dcc3852b6ee3c8b6dbd2
joaomanaia <joaopfmanaia@gmail.com>
2026-04-05 13:28:02 +0100

    Update dependencies.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite"
 
 import react from "@astrojs/react"
 
+import icon from "astro-icon"
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), icon()],
 })
