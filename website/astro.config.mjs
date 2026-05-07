a291cc6f5427e30f3393a9f88b88a56ed5146484
joaomanaia <joaopfmanaia@gmail.com>
2026-05-07 15:11:47 +0100

    Add sitemap

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,12 +5,14 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react"
 
 import icon from "astro-icon"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joaomanaia.dev",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), sitemap()],
 })
