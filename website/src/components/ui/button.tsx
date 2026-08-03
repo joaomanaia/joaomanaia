dd55b27db5206d0bf33ba718ba7447a0c96bfade
joaomanaia <joaopfmanaia@gmail.com>
2026-08-03 17:18:09 +0100

    Improve the UI

--- website/src/components/ui/button.tsx
+++ website/src/components/ui/button.tsx
@@ -11,6 +11,8 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/80",
         outline:
           "border-border bg-background hover:bg-muted hover:text-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 aria-expanded:bg-muted aria-expanded:text-foreground shadow-xs",
+        outlineExpressive:
+          "border-border bg-background hover:bg-primary hover:text-primary-foreground dark:border-input shadow-xs",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
         ghost:
